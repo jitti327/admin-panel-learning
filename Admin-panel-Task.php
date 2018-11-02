3547e4d30954221d20aedaf44b2564679978242a
shubham-ids <shubhamrajput009@gmail.com>
2018-11-02 17:36:59 +0530

    TODO task for admin panel

---
+++ Admin-panel-Task.php
@@ -0,0 +1,13 @@
+Holiday Task:
+1.{ Admin }
+  Admin Email    : shubham@gmail.com
+  Admin Password : incredible  
+2.
+  => Admin can block of the users.
+  => Admin can delete of the users.
+3.{ User }
+  => user can registered the value and then checked the activity checkbox
+  => user are go to listing page or show of the status.
+  => user can not checked the activity then user are go to demo page.
+  => Admin can blocked of the user and then user are not diplay the listing page.
+  =>user goto demo age.
