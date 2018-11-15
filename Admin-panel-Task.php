5bbe13c5aa5c907f0449a034cdbd3340cc28ffd5
jitti327 <jitti327@gmail.com>
2018-11-15 18:07:53 +0530

    New Task For Weekends 11/15/2018

--- Admin-panel-Task.php
+++ Admin-panel-Task.php
@@ -13,9 +13,44 @@ Holiday Task:
   => Admin can blocked of the user and then user are not diplay the listing page.
   =>user goto demo age.
 
-/***** New Task *****/
-
 1. Display Error message in " Search Not Found Value "
 2. Display selected value in drop down by table listing
 3. diisplay the order icon in one time to one icon
 4. not proper work in JqueryAjax in block person
+
+/***** New Task For Weekends *****/
+
+Make a sreen in which you are going to list
+
+
+--> Total number of subjects
+--> Total number of users -- (blocked and active)
+
+    Total number user  : 60
+    Total number unblocked user  : 20
+    Total number of blocked user : 40
+    Total number of online user  : 10 ( think on this )
+
+
+--> Total number of classes added
+  
+    Total number of classes - 40
+--> Maximum number of theortical marks for a subject in each class 
+
+  B.tech - Maths   ( 40)
+  B.Sc   - Science ( 80)
+
+--> Last added Class
+--> Last added Subject
+
+--> Recent 5 user who have registerd
+
+
+--> In registration page add these fields
+
+   --> Country
+   --> State
+   --> City
+
+--> In admin panel make 3 tabs to manage, city , state and country
+
